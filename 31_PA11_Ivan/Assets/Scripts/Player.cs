7aa64ee8d2e42d6af0d5891a8f5336b2baed21f8
ayyyvannn <64124308+ayyyvannn@users.noreply.github.com>
2020-06-18 19:36:42 +0800

    Uploaded by Ivan

---
+++ 31_PA11_Ivan/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public int score;
+
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider others)
+    {
+        if (others.gameObject.tag == "scoreObject")
+        {
+            score += 1;
+
+            ScoreText.text = "Score: " + score;
+        }
+    }
+}
---
+++ 31_PA11_Ivan/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject red;
+    public GameObject green;
+    public GameObject brown;
+
+    float randomNumber;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+
+        randomNumber = Random.Range(1, 4);
+
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+
+        if (randomNumber == 1)
+        {
+            Instantiate(red, transform.position, transform.rotation);
+        }
+
+        if (randomNumber == 2)
+        {
+            Instantiate(green, transform.position, transform.rotation);
+        }
+
+        if (randomNumber == 3)
+        {
+            Instantiate(brown, transform.position, transform.rotation);
+        }
+
+    }
+}
